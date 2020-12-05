dba937f26473c044f049b45e69ae0fa5d5dbb65c
Nico Guth <nico.guth@tu-dortmund.de>
2020-12-05 19:36:25 +0100

    added fixed_dt and debug_time

--- constants.py
+++ constants.py
@@ -3,6 +3,8 @@ animation_start_delay = 2 # how many seconds the animation should stand still at
 animation_time = 100 # how many seconds should the animation run
 animation_end_delay = 2 # how many seconds the animation should stand still at the end
 fps = 60 # frames per second
+fixed_dt = False # when saved as a video you'll want a fixed dt
+show_debug_time = False
 
 # image constants
 image_path = "images/python_logo_32.png"
