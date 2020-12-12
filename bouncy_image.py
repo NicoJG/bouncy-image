c3fe4ebdc023f1051379ac3e0c2b04d7718c88a2
Nico Guth <nico.guth@tu-dortmund.de>
2020-12-12 16:32:00 +0100

    file_output working but kinda buggy and janky

--- bouncy_image.py
+++ bouncy_image.py
@@ -2,6 +2,8 @@ import numpy as np
 import random
 import matplotlib.pyplot as plt
 import matplotlib.animation as anim
+from matplotlib.axes import Axes
+from  matplotlib.figure import Figure
 import time
 from PIL import Image
 
@@ -12,26 +14,59 @@ from boundary import Boundary
 
 # TODO: collisions are kinda weird when multiple collisions happen to one ball on one frame
 
+# init figure and axes
+fig = plt.figure()
+ax = fig.add_subplot()
+# get image pixels
+img = Image.open(image_path)
+pix = img.load()
+# set boundaries
+boundary = Boundary(-1,img.size[0],-1,img.size[1])
+# reduce white margins
+fig.tight_layout()
+# visible axis?
+if not show_axis: 
+    ax.axis("off")
+    fig.subplots_adjust(left=0, bottom=0, right=1, top=1, wspace=0, hspace=0)
+# set axes limits
+ax.set_xlim(boundary.l, boundary.r)
+ax.set_ylim(boundary.b, boundary.t)
+# set figure size
+aspect_ratio = img.width/img.height
+fig.set_size_inches(fig_height*aspect_ratio, fig_height, True)
+# set fig dpi (buggy: only set when not saving)
+if not save_video: fig.set_dpi(fig_dpi)
+
+# init plot before the init_animation because scat.set_array() does not work for setting the colors
+# init balls
+balls = []
+# init balls
+for col in range(img.size[0]):
+    for row in range(img.size[1]):
+        # TODO: don't use white/grey/transparent pixels
+        if pix[col,row][3] > 0:
+            x = float(col)
+            y = float(img.size[1] - row - 1)
+            color = [rgba/255 for rgba in pix[col,row]]
+            ball = Ball(x,y,color)
+            balls.append(ball)
+# init plot data
+x_data = []
+y_data = []
+color_data = []
+for ball in balls:
+    x_data.append(ball.x[0])
+    y_data.append(ball.x[1])
+    color_data.append(ball.color)
+
 # init plot
-fig,ax = plt.subplots()
+scat = ax.scatter(x_data,y_data, c=color_data)
 
 # init_func called before the first frame
 def init_animation():
-    global balls, boundary, scat
+    global balls, scat
     # init balls
     balls = []
-    # get image pixels
-    img = Image.open(image_path)
-    pix = img.load()
-    # set boundaries
-    boundary = Boundary(-1,img.size[0],-1,img.size[1])
-    # set figure size
-    fig.set_figwidth(fig_width)
-    aspect_ratio = img.width/img.height
-    fig.set_figheight(fig_width/aspect_ratio)
-    # set axes limits
-    ax.set_xlim(boundary.l, boundary.r)
-    ax.set_ylim(boundary.b, boundary.t)
     # init balls
     for col in range(img.size[0]):
         for row in range(img.size[1]):
@@ -45,27 +80,31 @@ def init_animation():
     # init plot data
     x_data = []
     y_data = []
-    color_data = []
     for ball in balls:
         x_data.append(ball.x[0])
         y_data.append(ball.x[1])
-        color_data.append(ball.color)
 
     # calc ball size in points^2 via matplotlib transformations
     r_ = ax.transData.transform([r,0])[0] - ax.transData.transform([0,0])[0] # points
     marker_size = (2*r_)**2 * marker_size_correction_factor # points^2
     # init plot
-    scat = ax.scatter(x_data, y_data, c=color_data, s=marker_size)
+    scat.set_offsets(np.column_stack((x_data,y_data)))
+    scat.set_sizes(np.array([marker_size]))
 
     return scat,
 
 # generator function for each frames delta time
 def calc_dt():
+    global frame_count
+    frame_count = 0
     starting_time = time.time()
     current_time = starting_time-1/fps
     while True:
         last_time = current_time
-        current_time = time.time()
+        if fixed_dt:
+            current_time += 1/fps
+        else:
+            current_time = time.time()
         if current_time - starting_time <= animation_start_delay:
             dt = 0.
         elif current_time - starting_time <= animation_start_delay + animation_time:
@@ -77,6 +116,7 @@ def calc_dt():
             dt = 0.
         else:
             break
+        frame_count += 1
         yield dt
 
 # does the animation
@@ -113,7 +153,7 @@ def update_animation(dt):
         x_data.append(ball.x[0])
         y_data.append(ball.x[1])
     scat.set_offsets(np.column_stack((x_data,y_data)))
-    
+
     debug_time("visuals",show_debug_time)
 
     return scat,
@@ -126,7 +166,16 @@ def debug_time(label="elapsed-time",show_msg=True):
         print(label," = ",time.time()-debug_time_last)
     debug_time_last = time.time()
 
-# animate
-ani = anim.FuncAnimation(fig, update_animation, frames=calc_dt, init_func=init_animation, interval=1000/fps, blit=True)
+# estimate frame count
+frame_count_estimate = fps * (animation_start_delay + animation_time + animation_end_delay)
 
-plt.show()
+# animate
+ani = anim.FuncAnimation(fig, update_animation, frames=calc_dt, init_func=init_animation, interval=1000/fps, save_count=frame_count_estimate, blit=True, repeat=False)
+
+if save_video: 
+    if save_as_gif:
+        ani.save(video_file+".gif", writer="pillow", dpi=fig_dpi, fps=fps)
+    else:
+        ani.save(video_file+".mp4", writer="ffmpeg", dpi=fig_dpi, fps=fps)
+else:
+    plt.show()
--- constants.py
+++ constants.py
@@ -1,17 +1,25 @@
+# video constants
+save_video = True
+save_as_gif = True
+video_file = "video" # without ending
+video_height = 1080 # pixels
+
 # figure constants
-fig_width = 8 # Inches
-marker_size_correction_factor = 0.5
+fig_height = 10 # Inches (don't change! it messes with the resolution for saved videos)
+fig_dpi = video_height/fig_height
+show_axis = False
+marker_size_correction_factor = 0.45 # just try it out with r=0.5 and "guess" the right factor
 
 # animation constants
 animation_start_delay = 2 # how many seconds the animation should stand still at the beginning
-animation_time = 100 # how many seconds should the animation run
+animation_time = 10 # how many seconds should the animation run
 animation_end_delay = 2 # how many seconds the animation should stand still at the end
-fps = 60 # frames per second
-fixed_dt = False # when saved as a video you'll want a fixed dt
+fps = 30 # frames per second
+fixed_dt = True # when saved as a video you'll want a fixed dt
 show_debug_time = False
 
 # image constants
-image_path = "images/python_logo_32.png"
+image_path = "images/python_logo_64.png"
 
 # ball constants
 r = 0.35 # radius
